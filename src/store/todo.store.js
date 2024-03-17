722599bf02fc1b31aede54d43c3b94b3ee90accf
Mounaji <mounajiamin@gmail.com>
2024-03-17 16:21:13 +0100

    añadimos métodos vacios del store video 112

--- src/store/todo.store.js
+++ src/store/todo.store.js
@@ -21,6 +21,49 @@ const initStore = () => {
     console.log('InitStore');
 }
 
+const loadStore = () =>{
+    throw new Error('Not implemented');
+}
+
+/**
+ * 
+ * @param {String} descripcion 
+ */
+const addTodo = (descripcion) => {
+    throw new Error('Not implemented');
+}
+
+/**
+ * 
+ * @param {String} todoId Tofo identificador
+ */
+
+const toggleTodo =  ( todoId )  => {
+    throw new Error('Not implemented');
+}
+
+const deleteTodo = ( todoId) => {
+    throw new Error('Not implemented');
+}
+
+const deleteCompleted = () => {
+    throw new Error('Not implemented');
+}
+
+const setFilter = (newFilter = Filters.All) => {
+    throw new Error('Not implemented');
+}
+
+const getCurrentFilter = () => {
+    throw new Error('Not implemented');
+}
+
 export default {
     initStore,
+    loadStore,
+    toggleTodo,
+    deleteTodo,
+    deleteCompleted,
+    setFilter,
+    getCurrentFilter,
 }
